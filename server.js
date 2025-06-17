efaf7753b5d7a2477d04dfb156c584c6af6ea6a8
Alex Eads <alexeads@Alexs-MacBook-Air-3.local>
2025-06-16 21:25:09 -0400

    fjs

--- server.js
+++ server.js
@@ -5,12 +5,8 @@ const pizzas = require("./data/pizzas");
 const pizzaSchema = require("./validation/pizzaSchema");
 
 app.use(cors());
-app.use(express.static("public"));
 app.use(express.json()); // to parse JSON bodies
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
 
 app.get("/api/pizzas", (req, res) => {
   res.json(pizzas);
@@ -23,7 +19,6 @@ app.post("/api/pizzas", (req, res) => {
     return res.status(400).json({ error: error.details[0].message });
   }
 
-  // Assign a unique _id for the new pizza
   const newPizza = {
     _id: pizzas.length > 0 ? pizzas[pizzas.length - 1]._id + 1 : 1,
     ...value,
