cb746f887478706efaa0fbc5ec7dddd5caa74196
Alex Eads <alexeads@Alexs-MacBook-Air-3.local>
2025-06-16 21:12:11 -0400

    gsjb

--- validation/pizzaSchema.js
+++ validation/pizzaSchema.js
@@ -1,7 +1,6 @@
 const Joi = require("joi");
 
 const pizzaSchema = Joi.object({
-  _id: Joi.number().required(),
   name: Joi.string().min(2).required(),
   size: Joi.array().items(Joi.string()).required(),
   price: Joi.array().items(Joi.string()).required(),
